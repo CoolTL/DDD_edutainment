bd1bc9ba71dc6cb9ab11b30c3f88ecea9fe0b100
CoolTL <thomasliisborg@gmail.com>
2026-01-09 09:32:26 +0100

    made some small test stuff

---
+++ draggable.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+var dragging = false
+var of = Vector2(0,0)
+var start_pos = 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if dragging:
+		position = get_global_mouse_position() - of
+	
+
+func _on_button_button_down() -> void:
+	start_pos = global_position
+	dragging = true
+	of = get_global_mouse_position() - global_position
+
+func _on_button_button_up() -> void:
+	dragging = false
+	position = start_pos
