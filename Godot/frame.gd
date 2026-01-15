f65869ba0c722c79e3297da8d472c33ff5ad7266
CoolTL <thomasliisborg@gmail.com>
2026-01-15 13:30:21 +0100

    done a bunch of shit

---
+++ Godot/frame.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func hide_background():
+	$TextureRect.hide()
+
+func _on_area_exited(area: Area2D) -> void:
+	if area.get_parent().dragging == false:
+		if area.get_parent().back_index == 0:
+			hide_background()
+			$TextureRect.show()
+		elif area.get_parent().back_index == 1:
+			hide_background()
+			print("works")
