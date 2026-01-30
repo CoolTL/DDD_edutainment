e3c014d25c8c61de4cc272c25747c58ba2a17a53
CoolTL <thomasliisborg@gmail.com>
2026-01-30 09:22:05 +0100

    worked more on frame 4 of level 1

--- Godot/scenes/char_spot_4.gd
+++ Godot/scenes/char_spot_4.gd
@@ -3,6 +3,11 @@ extends Area2D
 # We use this signal even though we have the check button to make the frame register if its solved or not
 signal changed
 
+@export var correct_text = ""
+@export var wrong_text = ""
+var clear_text = ""
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,14 +18,16 @@ func _process(_delta: float) -> void:
 	pass
 
 func clear_spot():
-	$ColorRect.hide()
-	$ColorRect2.hide()
+	$Label.text = clear_text
 
 func _on_area_exited(area: Area2D) -> void:
 	if area.get_parent().dragging == false:
 		if area.get_parent().char_index == 0:
 			clear_spot()
-			$ColorRect.show()
+			$Label.text = correct_text
 		elif area.get_parent().char_index == 1:
+			clear_spot()
+			$Label.text = wrong_text
+		else:
 			clear_spot()
 		changed.emit()
