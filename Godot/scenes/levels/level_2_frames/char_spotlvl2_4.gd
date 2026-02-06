5306d07e162af35562f422c33d29169966f1aaf9
NEXT15930 <fake@email.com>
2026-02-06 10:31:08 +0100

    Level 2 frame 4

--- Godot/scenes/levels/level_2_frames/char_spotlvl2_4.gd
+++ Godot/scenes/levels/level_2_frames/char_spotlvl2_4.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export var correct_msg = ""
-@export var wrong_msg = ""
+
 
 # We use this signal even though we have the check button to make the frame register if its solved or not
 signal changed
@@ -23,10 +23,16 @@ func _on_area_exited(area: Area2D) -> void:
 	if area.get_parent().dragging == false:
 		if area.get_parent().char_index == 0:
 			clear_spot()
-			$Label.text = correct_msg
+			$Label.text = "25 skibe sunket"
 		elif area.get_parent().char_index == 1:
 			clear_spot()
-			$Label.text = wrong_msg
+			$Label.text = "28 skibe sunket"
+		elif area.get_parent().char_index == 2:
+			clear_spot()
+			$Label.text = correct_msg
+		elif area.get_parent().char_index == 3:
+			clear_spot()
+			$Label.text = "36 skibe sunket"
 		else:
 			clear_spot()
 		changed.emit()
